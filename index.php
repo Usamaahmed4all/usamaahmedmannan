d759364f16eb0e1b7d6423ba95643c856adc92d8
Usamaahmed4all <30299982+Usamaahmed4all@users.noreply.github.com>
2017-07-20 00:12:24 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,78 +1,50 @@
+<center><h1>
 <?php
-ini_set('display_errors',0);
-if( isset( $_REQUEST['calculate'] ))
+
+$name=”shree”;
+$regno=1;
+
+$s1=40;
+$s2=20;
+$s3=30;
+
+$t=$s1+$s2+$s3;
+$p=$t/3;
+
+if($p>=60)
 {
-$operator=$_REQUEST['operator'];
-if($operator=="+")
+echo ” first division “;
+}
+if($p>=50 and $p<60)
 {
-$add1 = $_REQUEST['fvalue'];
-$add2 = $_REQUEST['lvalue'];
-$res= $add1+$add2;
+echo ” Second division “;
 }
-if($operator=="-")
+if($p>=40 and $p<50)
 {
-$add1 = $_REQUEST['fvalue'];
-$add2 = $_REQUEST['lvalue'];
-$res= $add1-$add2;
+echo ” Third division “;
+
 }
-if($operator=="*")
+
+if($p<40)
 {
-$add1 = $_REQUEST['fvalue'];
-$add2 = $_REQUEST['lvalue'];
-$res =$add1*$add2;
+echo ” fail”;
+
 }
-if($operator=="/")
+
+ 
+
+echo “<br>”;
+if($s1>=35 and $s2>=35 and $s3>=35)
 {
-$add1 = $_REQUEST['fvalue'];
-$add2 = $_REQUEST['lvalue'];
-$res= $add1/$add2;
+$r=”pass”;
 }
-if($_REQUEST['fvalue']==NULL && $_REQUEST['lvalue']==NULL)
+else
 {
-echo "<script language=javascript> alert(\"Please Enter values.\");</script>";
+$r=”fail”;
 }
-else if($_REQUEST['fvalue']==NULL)
-{
-echo "<script language=javascript> alert(\"Please Enter First value.\");</script>";
-}
-else if($_REQUEST['lvalue']==NULL)
-{
-echo "<script language=javascript> alert(\"Please Enter second value.\");</script>";
-}
-}
+
+echo $r;
+echo $t;
+echo $p;
+
 ?>
-<form>
-<table style="border:groove #00FF99">
-            <tr>
-                <td style="background-color:aqua; color:red; font-family:'Times New Roman'">Enter First Number</td>
-                <td colspan="1">
-               
-                    <input name="fvalue" type="text" style="color:red"/></td>
-            <tr>
-                <td style="color:burlywood; font-family:'Times New Roman'">Select Operator</td>
-                <td>
-                    <select name="operator" style="width: 63px">
-<option>+</option>
-<option>-</option>
-<option>*</option>
-<option>/</option>
-</select></td>
-               </tr>
-            <tr>
-                <td style="background-color:aqua; color:red; font-family:'Times New Roman'">Enter First Number</td>
-                <td class="auto-style5">
-                    <input name="lvalue" type="text"  style="color:red"/></td>
-               
-            </tr>
-            <tr>
-                <td></td>
-                <td><input type="submit" name="calculate" value="Calculate" style="color:wheat;background-color:rosybrown" /></td>
-               
-            </tr>
-            <tr>
-                <td style="background-color:aqua;color:red">Output = </td>
-                <td style="color:darkblue"><?php echo $res;?></td>
-               
-            </tr>
-       </table>
- </form>
